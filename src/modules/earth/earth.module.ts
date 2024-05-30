1281cbfc75912dac19edb5a831e1dbc329f29630
Raphael Kauan <raphaelkauanoficial@gmail.com>
2024-05-30 13:10:13 -0400

    small correction

--- src/modules/earth/earth.module.ts
+++ src/modules/earth/earth.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { EarthController } from "./earth.controller";
 import { EarthService } from "./earth.service";
 import { PrismaService } from "src/database/prisma.service";
+import { EarthRepository } from "./repository/earth.repository";
 
 @Module({
     controllers: [EarthController],
-    providers: [EarthService, PrismaService],
+    providers: [EarthService, PrismaService, EarthRepository],
 })
 export class EarthModule {}
